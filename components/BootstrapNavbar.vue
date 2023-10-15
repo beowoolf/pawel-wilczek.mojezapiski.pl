9df60e47684c04e4d0f90f8cdbb8642855c13ac8
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2023-10-15 09:00:53 +0200

    My business card

---
+++ components/BootstrapNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav
+    class="navbar"
+    :class="
+      'navbar-' + type + ' bg-' + variant + ' navbar-expand-' + toggleable
+    "
+  >
+    <slot />
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'BootstrapNavbar',
+  props: {
+    toggleable: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    variant: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ components/StickyFooter.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class="fixed-bottom font-size-sticky-footer py-2"
+    :class="
+      $colorMode.preference == 'light' ? 'bg-footer-dark' : 'bg-footer-dark'
+    "
+  >
+    <slot>{{ text }}</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StickyFooter',
+  props: {
+    text: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style>
+.bg-footer-light {
+  background-color: rgb(210 210 210 / 100%) !important;
+  color: rgba(0 0 0 / 80%);
+}
+
+.bg-footer-dark {
+  background-color: rgb(48 49 59 / 100%) !important;
+  color: #ebf4f1;
+}
+
+@media only screen and (max-width: 767px) {
+  .font-size-sticky-footer {
+    font-family: Poppins, Arial, Verdana, Tahoma, sans-serif;
+    font-weight: normal;
+    font-size: 10px;
+    text-align: center;
+  }
+}
+@media (min-width: 768px) {
+  .font-size-sticky-footer {
+    font-family: Poppins, Arial, Verdana, Tahoma, sans-serif;
+    font-weight: normal;
+    font-size: 14px;
+    text-align: center;
+  }
+}
+</style>
