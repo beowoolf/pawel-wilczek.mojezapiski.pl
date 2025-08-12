13c5c9100114fc3738497ec95f76b2fe1758ef9e
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2025-08-12 19:02:27 +0200

    Update update.bash

--- update.bash
+++ update.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir -p ~/bin && ln -fs /usr/local/bin/node18 ~/bin/node && ln -fs /usr/local/bin/npm18 ~/bin/npm && source $HOME/.bash_profile
+mkdir -p ~/bin && ln -fs /usr/local/bin/node20 ~/bin/node && ln -fs /usr/local/bin/npm20 ~/bin/npm && source $HOME/.bash_profile
 git pull && yarn && yarn build:prod -m -t && devil www restart pawel-wilczek.mojezapiski.pl
